59e0eddea9292f666285a28118312135126f669e
onielrosario <onielrosario@pursuit.org>
2019-08-09 16:46:29 -0600

    API models created

--- HealthFirstChallengeAssesment/Models/Models.swift
+++ HealthFirstChallengeAssesment/Models/Models.swift
@@ -14,10 +14,29 @@ import Foundation
  */
 
 class People: Codable {
-    
+    let next: URL?
+    let previous: URL?
+    class PeopleResult: Codable {
+        // people requested information : Name,Hair color,Eye color,Birth year,Date created
+        let name: String
+        let hair_color: String
+        let eye_color: String
+        let birth_year: String
+        let created: String
+    }
+    let results: [PeopleResult]
 }
 
 
 class Planets: Codable {
-    
+    let next: URL?
+    let previous: URL?
+    class PlanetsResult: Codable {
+        // planet requested information: Name,Climate,Population,Date created
+        let name: String
+        let climate: String
+        let population: String
+        let created: String
+    }
+    let results: [PlanetsResult]
 }
