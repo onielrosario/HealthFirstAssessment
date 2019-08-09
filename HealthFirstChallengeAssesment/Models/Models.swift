4f9015a04569d9c0a8ce3be9580e2bfc4e2f7421
onielrosario <onielrosario@pursuit.org>
2019-08-09 15:49:35 -0600

    create API models and unit testing the urls, added git ignore files for DS_Store

---
+++ HealthFirstChallengeAssesment/Models/Models.swift
@@ -0,0 +1,23 @@
+//
+//  People.swift
+//  HealthFirstChallengeAssesment
+//
+//  Created by Oniel Rosario on 8/9/19.
+//  Copyright © 2019 Oniel Rosario. All rights reserved.
+//
+
+import Foundation
+
+/*
+ will create classes instead of struct
+ to be able to cache once I display Data
+ */
+
+class People: Codable {
+    
+}
+
+
+class Planets: Codable {
+    
+}
--- HealthFirstChallengeAssesmentTests/HealthFirstChallengeAssesmentTests.swift
+++ HealthFirstChallengeAssesmentTests/HealthFirstChallengeAssesmentTests.swift
@@ -19,16 +19,14 @@ class HealthFirstChallengeAssesmentTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    //testing the  STAR WARS APIs
+    func testGETpeopleAPI() {
+    let exp = expectation(description: "received data")
+    let peopleUrl = "https://swapi.co/api/people/"
+    
+        
+    
+    wait(for: [exp], timeout: 3)
     }
 
 }
